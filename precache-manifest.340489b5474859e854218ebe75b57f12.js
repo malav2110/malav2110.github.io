2f5d5c3feea768493ac093dc0bd9fd6cc86d9110
Malav Shah <mshah@unicon.net>
2021-06-13 13:14:07 -0700

    Initial Commit

---
+++ precache-manifest.340489b5474859e854218ebe75b57f12.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaf495c548e8b0b32a43fef55ad825b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e91166279435200378a",
+    "url": "/static/css/main.d7604045.chunk.css"
+  },
+  {
+    "revision": "12db4930cf63a56dac74",
+    "url": "/static/js/2.69e2aea7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.69e2aea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e91166279435200378a",
+    "url": "/static/js/main.069f837d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f8deaab8764b5aeaac01bbbf735b5cef",
+    "url": "/static/media/asu-2.f8deaab8.png"
+  },
+  {
+    "revision": "090e354901d5fffe8e24a2a18cec419b",
+    "url": "/static/media/asu.090e3549.png"
+  },
+  {
+    "revision": "457f2c3449af483a25cc2e457bad81e6",
+    "url": "/static/media/malavshah.457f2c34.jpg"
+  },
+  {
+    "revision": "8748f117ee5f0a3dab1570f26f1e3c10",
+    "url": "/static/media/nirma.8748f117.png"
+  },
+  {
+    "revision": "7735c991bf04b3c27367bbc96baa2b27",
+    "url": "/static/media/resume.7735c991.pdf"
+  },
+  {
+    "revision": "7c93d5de16140e72b5108fd6424c87b7",
+    "url": "/static/media/technostacks-2.7c93d5de.png"
+  },
+  {
+    "revision": "b4dd7253fcb4ab67329299b297c82621",
+    "url": "/static/media/unicon.b4dd7253.png"
+  }
+]);
