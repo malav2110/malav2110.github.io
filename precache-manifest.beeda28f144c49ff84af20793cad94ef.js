574659f4e758633304202ea881cd373108714ff4
Malav Shah <mshah@unicon.net>
2023-09-22 14:21:10 -0700

    2023-updates

---
+++ precache-manifest.beeda28f144c49ff84af20793cad94ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5c179d1f6e90ea2fbc9a5f6a45b5d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f281bd3dc3fd27129dc1",
+    "url": "/static/css/main.6b6e7380.chunk.css"
+  },
+  {
+    "revision": "138f3326307728164c30",
+    "url": "/static/js/2.fbd3f895.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.fbd3f895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f281bd3dc3fd27129dc1",
+    "url": "/static/js/main.b09d1fd0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e797d37b2c9a6fb2632c940b13797b72",
+    "url": "/static/media/Unicon_Logo_Plain_Version_PNG_Vector_Vrs.e797d37b.png"
+  },
+  {
+    "revision": "f8deaab8764b5aeaac01bbbf735b5cef",
+    "url": "/static/media/asu-2.f8deaab8.png"
+  },
+  {
+    "revision": "090e354901d5fffe8e24a2a18cec419b",
+    "url": "/static/media/asu.090e3549.png"
+  },
+  {
+    "revision": "457f2c3449af483a25cc2e457bad81e6",
+    "url": "/static/media/malavshah.457f2c34.jpg"
+  },
+  {
+    "revision": "8748f117ee5f0a3dab1570f26f1e3c10",
+    "url": "/static/media/nirma.8748f117.png"
+  },
+  {
+    "revision": "7c93d5de16140e72b5108fd6424c87b7",
+    "url": "/static/media/technostacks-2.7c93d5de.png"
+  }
+]);
